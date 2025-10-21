43faa8bc78d5059d079f9544198452d13ed5df25
Yakov <itmeetm@gmail.com>
2025-10-21 13:59:16 +0300

    post

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/', summary="Главная ручка", tags=['Основные Ручки'])
-def root():
-    return "Hello World"
+
+books = [
+    {
+        'id': 1,
+        'title': 'Асинхронность в Python',
+        'author': 'Пушкин',
+    },
+    {
+        'id': 2,
+        'title': 'Backend в Python',
+        'author': 'Артем'
+    }
+]
+
+@app.get(
+    '/books',
+    tags=["Книги"],
+    summary="Получить все книги"
+)
+def read_books():
+    return books
+
+@app.get('/books/{books_id}',  tags=["Книги"], summary="Получить книгу")
+def get_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id: 
+            return book
+        raise HTTPException(status_code=404, detail="Книга не найдена")
+
+
+class NewBook(BaseModel):
+    title: str
+    author: str
+
+@app.post("/books", tags=["Книги"])
+def create_book(new_book: NewBook):
+    books.append({
+        "id": len(books) + 1,
+        "title": new_book.title,
+        "author": new_book.author
+    })
+    return {"success: true"}
+    
 
 if __name__ == '__main__':
     uvicorn.run("main:app", reload=True)
