72dd7fb88a1115cd5ca5528a22c14fe574f9a86a
Yakov <itmeetm@gmail.com>
2025-10-23 11:18:57 +0300

    CORS

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from src.api import main_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"]
+)
+
 app.include_router(main_router)
 
         
